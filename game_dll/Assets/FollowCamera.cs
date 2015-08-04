2289c9a83fcee211e1446e7e94d5c8464b9ba4da
GengZhengLin <gzl8042@163.com>
2015-08-04 15:50:25 -0700

    level 3:

---
+++ game_dll/Assets/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject m_camera;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = m_camera.transform.position + new Vector3 (0, 5, 0);
+	}
+}
