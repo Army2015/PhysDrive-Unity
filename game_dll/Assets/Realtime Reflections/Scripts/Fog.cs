61f05f79fdfff244962e194fd1721520cfd903ca
unknown <hdvazquez@convergence.PHYSBAM>
2015-08-06 20:06:01 +1000

    added level 6

---
+++ game_dll/Assets/Realtime Reflections/Scripts/Fog.cs
@@ -0,0 +1,32 @@
+﻿
+//source: http://stackoverflow.com/questions/14553672/fog-in-unity-with-c-sharp
+
+using UnityEngine;
+using System.Collections;
+
+public class Fog : MonoBehaviour {
+	int count = 0;
+	void Start () 
+	{
+		RenderSettings.fog =true;
+		RenderSettings.fogDensity = 2.00f;
+	}
+	
+	void update() 
+	{
+		count++;
+		float num = Random.value;
+		Debug.Log(num);
+		if (count % 2 != 0)
+		{ 
+			RenderSettings.fogDensity+=num;
+
+		}
+		else
+		{ 
+			RenderSettings.fogDensity-=num;
+		}
+
+	}
+
+}
